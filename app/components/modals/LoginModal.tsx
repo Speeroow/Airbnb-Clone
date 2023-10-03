11fe3a30312cb243ac6dbf7860e601d30ed85e8f
Speeroow <abhishek.rajawat1909@gmail.com>
2023-10-03 05:49:50 +0530

    completed with login

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { signIn } from "next-auth/react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import {FcGoogle} from 'react-icons/fc';
@@ -9,20 +10,24 @@ import {
     SubmitHandler,
     useForm
 } from 'react-hook-form';
+import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import toast from "react-hot-toast";
 import Button from "../Button";
-import {signIn} from 'next-auth/react';
+import { useRouter } from "next/navigation";
 
 
 
 
 
-const RegisterModal = () => {
+
+const LoginModal = () => {
+    const router = useRouter();
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -33,7 +38,7 @@ const RegisterModal = () => {
         }
     } = useForm<FieldValues>({
         defaultValues: {
-            name:'',
+            // name:'',
             email:'',
             password:''
         }
@@ -42,32 +47,39 @@ const RegisterModal = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data)=>{
         setIsLoading(true);
 
-        axios.post('/api/register', data)
-            .then(()=>{
-                registerModal.onClose();
-            })
-            .catch((error)=>{
-                toast.error('Something went wrong');
-            })
-            .finally(()=>{
-                setIsLoading(false);
-            })
+       signIn('credentials', {
+        ...data,
+        redirect: false,
+       })
+       .then((callback)=>{
+        setIsLoading(false);
+
+        if(callback?.ok){
+            toast.success('Logged In');
+            router.refresh();
+            loginModal.onClose();
+        }
+
+        if(callback?.error){
+            toast.error(callback.error);
+        }
+       })
     }
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
             <Heading 
-                title="Welcome To Airbnb"
-                subtitle="Create an Account"
+                title="Welcome Back"
+                subtitle="Login to your Account"
             />
-            <Input
+            {/* <Input
                id='name'
                label="Name"
                disabled={isLoading}
                register={register}
                errors={errors}
                required             
-            />
+            /> */}
             <Input
                id='email'
                label="Email"
@@ -134,10 +146,10 @@ const RegisterModal = () => {
     return ( 
         <Modal
           disabled={isLoading}
-          isOpen = {registerModal.isOpen}
-          title="Register"
+          isOpen = {loginModal.isOpen}
+          title="Login"
           actionLabel="Continue"
-          onClose={registerModal.onClose} 
+          onClose={loginModal.onClose} 
           onSubmit={handleSubmit(onSubmit)} 
           body={bodyContent}
           footer={footerContent}
@@ -145,4 +157,4 @@ const RegisterModal = () => {
      );
 }
  
-export default RegisterModal;
+export default LoginModal;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,20 @@ import Avatar from './Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import {signOut} from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
-const UserMenu = () => {
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null
+}
+
+const UserMenu:React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(()=>{
@@ -52,7 +63,7 @@ const UserMenu = () => {
                 >
                     <AiOutlineMenu/>
                     <div className='hidden md:block'>
-                        <Avatar/>
+                        <Avatar src={currentUser?.image}/>
                     </div>
                 </div>
             </div>
@@ -73,9 +84,38 @@ const UserMenu = () => {
                 '
               >
                 <div className='flex flex-col cursor-pointer'>
-                  <>
+                  {currentUser ? (
+                    <>
                     <MenuItem
                       onClick={()=>{}}
+                      label='My trips'
+                     />
+                      <MenuItem
+                      onClick={()=>{}}
+                      label='My favorites'
+                     />
+                     <MenuItem
+                      onClick={()=>{}}
+                      label='My reservation'
+                     />
+                     <MenuItem
+                      onClick={()=>{}}
+                      label='My properties'
+                     />
+                     <MenuItem
+                      onClick={()=>{}}
+                      label='Airbnb my home'
+                     />
+                     <hr/>
+                     <MenuItem
+                      onClick={()=>signOut()}
+                      label='LogOut'
+                     />
+                  </>
+                  ) : (
+                    <>
+                    <MenuItem
+                      onClick={loginModal.onOpen}
                       label='Login'
                      />
                       <MenuItem
@@ -83,6 +123,7 @@ const UserMenu = () => {
                       label='Sign Up'
                      />
                   </>
+                  )}
                 </div>
               </div>
             )}
