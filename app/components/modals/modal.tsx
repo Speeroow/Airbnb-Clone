0c9e07fd4fc9ce8999409005e3b1fb9f5a3e3977
Speeroow <abhishek.rajawat1909@gmail.com>
2023-10-25 20:29:03 +0530

    completed

--- app/components/modals/modal.tsx
+++ app/components/modals/modal.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+
 import Button from "../Button";
 
 interface ModalProps {
@@ -23,8 +24,8 @@ const Modal: React.FC<ModalProps> = ({
   onSubmit,
   title,
   body,
-  footer,
   actionLabel,
+  footer,
   disabled,
   secondaryAction,
   secondaryActionLabel,
@@ -44,21 +45,23 @@ const Modal: React.FC<ModalProps> = ({
     setTimeout(() => {
       onClose();
     }, 300);
-  }, [disabled, onClose]);
+  }, [onClose, disabled]);
 
   const handleSubmit = useCallback(() => {
     if (disabled) {
       return;
     }
+
     onSubmit();
-  }, [disabled, onSubmit]);
+  }, [onSubmit, disabled]);
 
   const handleSecondaryAction = useCallback(() => {
     if (disabled || !secondaryAction) {
       return;
     }
+
     secondaryAction();
-  }, [disabled, secondaryAction]);
+  }, [secondaryAction, disabled]);
 
   if (!isOpen) {
     return null;
@@ -68,82 +71,107 @@ const Modal: React.FC<ModalProps> = ({
     <>
       <div
         className="
-                justify-center items-center flex overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70
-            "
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
       >
         <div
           className="
-                  relative
-                  w-full
-                  md:w-4/6
-                  lg:w-3/6
-                  xl:w-2/5
-                  my-6
-                  mx-auto
-                  h-full
-                  lg:h-auto
-                  md:h-auto  
-                "
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto 
+          h-full 
+          lg:h-auto
+          md:h-auto
+          "
         >
-          {/* CONTENT */}
+          {/*content*/}
           <div
             className={`
-                     translate
-                     duration-300
-                     h-full
-                     ${showModal ? "translate-y-0" : "translate-y-full"}
-                     ${showModal ? "opacity-100" : "opacity-0"}   
-                   `}
+            translate
+            duration-300
+            h-full
+            ${showModal ? "translate-y-0" : "translate-y-full"}
+            ${showModal ? "opacity-100" : "opacity-0"}
+          `}
           >
             <div
               className="
-                        translate
-                        h-full
-                        lg:h-auto
-                        md:h-auto
-                        border-0
-                        rounded-lg
-                        shadow-lg
-                        relative
-                        flex
-                        flex-col
-                        w-full
-                        bg-white
-                        outline-none
-                        focus:outline-none
-                     "
+              translate
+              h-full
+              lg:h-auto
+              md:h-auto
+              border-0 
+              rounded-lg 
+              shadow-lg 
+              relative 
+              flex 
+              flex-col 
+              w-full 
+              bg-white 
+              outline-none 
+              focus:outline-none
+            "
             >
-              {/* HEADER */}
+              {/*header*/}
               <div
                 className="
-                          flex
-                          items-center
-                          p-6
-                          rounded-t
-                          justify-center
-                          relative
-                          border-b-[1px]  
-                       "
+                flex 
+                items-center 
+                p-6
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
+                "
               >
                 <button
+                  className="
+                    p-1
+                    border-0 
+                    hover:opacity-70
+                    transition
+                    absolute
+                    left-9
+                  "
                   onClick={handleClose}
-                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
                 >
                   <IoMdClose size={18} />
                 </button>
                 <div className="text-lg font-semibold">{title}</div>
               </div>
-              {/* BODY */}
+              {/*body*/}
               <div className="relative p-6 flex-auto">{body}</div>
-              {/* FOOTER */}
+              {/*footer*/}
               <div className="flex flex-col gap-2 p-6">
-                <div className="flex flex-row items-center gap-4 w-full">
+                <div
+                  className="
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-4 
+                    w-full
+                  "
+                >
                   {secondaryAction && secondaryActionLabel && (
                     <Button
-                      outline
                       disabled={disabled}
                       label={secondaryActionLabel}
                       onClick={handleSecondaryAction}
+                      outline
                     />
                   )}
                   <Button
